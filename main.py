ac04ff025352150292b01d6133da1503530533af
Svetlana6757 <163926856+Svetlana6757@users.noreply.github.com>
2024-04-04 23:42:35 +0300

    add class Bird(Animal)

--- main.py
+++ main.py
@@ -2,3 +2,18 @@ class Animal:
     def __init__(self, name, age):
         self.name = name
         self.age = age
+
+
+    def make_sound(self):
+
+    def eat(self):
+        print(f"{self.name} сейчас ест.")
+
+class Bird(Animal):
+    def __init__(self, name, age):
+        super().__init__(name, age)
+
+    def make_sound(self):
+        print(f"{self.name} поет чирик-чирик")
+
+
